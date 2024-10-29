d8f5054f4492ef6616dd2de8602475f87b956b5d
Matteo <Matteo.lorenzoq@gmail.com>
2024-10-29 12:45:11 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+  session_start();
+
+  // Si on a pas de token CSRF, on en génère un
+  if (!isset($_SESSION['csrf_user_create']) || empty($_SESSION['csrf_user_create'])) {
+    $_SESSION['csrf_user_create'] = bin2hex(random_bytes(32));
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <title>Document</title>
+</head>
+<body class="bg-gray-100">
+  <h1 class="text-5xl text-center mt-20">Create your account :</h1>
+  <form action="traitement.php" method="POST" class="max-w-lg mx-auto mt-10 bg-white p-8 rounded-lg shadow-lg">
+    <div class="mb-4">
+      <label for="username" class="block text-gray-700 text-sm font-bold mb-2">Pseudo :</label>
+      <input type="text" name="username" id="username" placeholder="John Doe" required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+    </div>
+    <div class="mb-4">
+      <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Email :</label>
+      <input type="email" name="email" id="email" placeholder="john.doe@example.com" required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+    </div>
+    <div class="mb-4">
+      <label for="password" class="block text-gray-700 text-sm font-bold mb-2">Mot de passe :</label>
+      <input type="password" name="password" id="password" placeholder="A strong password" required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+    </div>
+    <div class="flex items-center justify-between">
+      <input type="submit" value="Ajouter" name="ajouter" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+    </div>
+    <input type="hidden" name="token" value="<?= $_SESSION["csrf_user_create"]; ?>">
+  </form>
+</body>
+</html>
---
+++ traitement.php
@@ -0,0 +1,74 @@
+<?php
+
+session_start();
+
+// Vérifie si le token CSRF est présent et valide
+if (!isset($_POST['token']) || empty($_POST['token']) || $_POST['token'] !== $_SESSION['csrf_user_create']) {
+  die('Token CSRF invalide');
+}
+
+// Supprime le token CSRF
+unset($_SESSION['csrf_user_create']);
+
+if (isset($_POST['username']) && !empty($_POST['username'])) {
+  $username = htmlspecialchars($_POST['username']);
+} else {
+  echo "<p>Le titre est obligatoire</p>";
+}
+
+if (isset($_POST['email']) && !empty($_POST['email'])) {
+  $email = htmlspecialchars($_POST['email']);
+} else {
+  echo "<p>Le contenu est obligatoire</p>";
+}
+
+if (isset($_POST['password']) && !empty($_POST['password'])) {
+  $password = htmlspecialchars($_POST['password']);
+} else {
+  echo "<p>Le password est obligatoire</p>";
+}
+
+
+if (isset($username) && isset($email) && isset($password)) {
+
+  // On hash le mot de passe
+  $password = password_hash($password, PASSWORD_DEFAULT, []);
+  // On génère un ID unique
+  $id = bin2hex(random_bytes(16));
+
+  // On se connecte à la base de données
+  require_once 'db.php';
+
+  // On prépare la requête
+  $sauvegarde = $pdo->prepare(
+    'INSERT INTO user_table (id, username, email, password) VALUES (:id, :username, :email, :password)'
+  );
+
+  // On exécute la requête
+  $sauvegarde->execute([
+    'id' => $id,
+    'username' => $username,
+    'email' => $email,
+    'password' => $password
+  ]);
+
+  // On vérifie si l'utilisateur a bien été ajouté
+  if($sauvegarde->rowCount() > 0) {
+    echo "<p>L'utilisateur a bien été ajouté</p>";
+    echo "<p>Voici les données de l'utilisateur ajouté : </p>";
+    echo "<p>ID : " . $id . "</p>";
+
+    session_start();
+    $_SESSION['user'] = $id;
+    $_SESSION['username'] = $username;
+
+    header('Location: me.php');
+
+    error_log("L'utilisateur " . $username . " a été ajouté avec l'ID : " . $id);
+
+  } else {
+    echo "<p>Une erreur est survenue</p>";
+  }
+}
+
+?>
